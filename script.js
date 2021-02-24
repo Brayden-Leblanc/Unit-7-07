a8c5b621802eb6d7151a4fca290f9bf069e2b3f3
BraydenLeblanc <brayden.leblanc@ocsbstudent.ca>
2021-02-24 16:02:54 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Creates variables
+document.getElementById('button').addEventListener('click', number)
+let first = 0
+let second = 0
+let total = 0
+
+function number () {
+  // Get user input from box
+  first = document.getElementById('input').value
+  second = document.getElementById('input2').value
+  
+  first = parseInt(first)
+  second = parseInt(second)
+
+  for (let i = 0; i < first; i++) {
+    total = total + second
+  }
+alert (total)
+}
